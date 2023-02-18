33de306e63457fcbaf09ebcf7938a4bede402ddc
ArturoAtomplay <64356325+ArturoAtomplay@users.noreply.github.com>
2023-02-18 00:27:52 +0000

    Fixed the addSubcommand and addSubcommandGroup options in the commands

--- src/structures/CommandBuilder.ts
+++ src/structures/CommandBuilder.ts
@@ -1,9 +1,37 @@
 import Bot from './Bot';
-import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
+import {
+  ChatInputCommandInteraction,
+  SharedSlashCommandOptions,
+  SlashCommandBuilder,
+  SlashCommandSubcommandBuilder,
+  SlashCommandSubcommandGroupBuilder,
+} from 'discord.js';
 
 export class CommandBuilder extends SlashCommandBuilder {
   cmdType = 'slash';
   run!: CommandFunction;
+
+  public addSubcommand(
+    input:
+      | SlashCommandSubcommandBuilder
+      | ((
+          subcommandGroup: SlashCommandSubcommandBuilder
+        ) => SlashCommandSubcommandBuilder)
+  ): SlashCommandSubcommandsOnlyBuilder {
+    super.addSubcommand(input);
+    return this;
+  }
+
+  public addSubcommandGroup(
+    input:
+      | SlashCommandSubcommandGroupBuilder
+      | ((
+          subcommandGroup: SlashCommandSubcommandGroupBuilder
+        ) => SlashCommandSubcommandGroupBuilder)
+  ): SlashCommandSubcommandsOnlyBuilder {
+    super.addSubcommandGroup(input);
+    return this;
+  }
 
   public setCallback(fn: CommandFunction) {
     this.run = fn;
@@ -11,6 +39,12 @@ export class CommandBuilder extends SlashCommandBuilder {
     return this;
   }
 }
+
+interface SlashCommandSubcommandsOnlyBuilder
+  extends Omit<
+    CommandBuilder,
+    Exclude<keyof SharedSlashCommandOptions, 'options'>
+  > {}
 
 type CommandFunction = (
   interaction: ChatInputCommandInteraction,
